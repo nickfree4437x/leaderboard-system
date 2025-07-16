10db0e24119d061b5b319a8f4656b05de2509d30
nickfree4437x <146987759+nickfree4437x@users.noreply.github.com>
2025-07-16 16:26:06 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import ClaimHistory from "./components/ClaimHistory"; // Assuming it's still in components
+import ClaimHistory from "./Components/ClaimHistory"; // Assuming it's still in components
 
 function App() {
   return (
